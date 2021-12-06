39e7eadf4c62f60ab41bf0cd8303aa8bc6e8cf08
Fertnam <fertnam@mail.ru>
2021-12-06 20:21:29 +1000

    Настройка рабочих инструментов

---
+++ src/components/App/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Container from '../Container/Container'
+import styles from './App.module.scss'
+
+function App() {
+    return (
+        <div className={styles.app}>
+            <Container>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Facilis, sequi.
+            </Container>
+        </div>
+    )
+}
+
+export default App
