5e91e60c3654d123d0058bbda729fb34f0111501
Fertnam <fertnam@mail.ru>
2021-12-10 22:59:52 +1000

    Верстка модерационной ленты

--- src/components/ModerationTape/index.tsx
+++ src/components/ModerationTape/index.tsx
@@ -30,6 +30,116 @@ function ModerationTape() {
                 bulletinText: 'Текст объявления ...',
                 bulletinImagees: [],
             },
+            {
+                id: 3242324,
+                publishDate: 2342343,
+                publishDateString: '12:46, сегодня',
+                ownerId: 44334,
+                ownerLogin: 'Sashok',
+                bulletinSubject: 'Web-программист',
+                bulletinText: 'Текст объявления ...',
+                bulletinImagees: [],
+            },
+            {
+                id: 32423214,
+                publishDate: 2342343,
+                publishDateString: '12:46, сегодня',
+                ownerId: 44334,
+                ownerLogin: 'Sashok',
+                bulletinSubject: 'Web-программист',
+                bulletinText: 'Текст объявления ...',
+                bulletinImagees: [],
+            },
+            {
+                id: 3242234,
+                publishDate: 2342343,
+                publishDateString: '12:46, сегодня',
+                ownerId: 44334,
+                ownerLogin: 'Sashok',
+                bulletinSubject: 'Web-программист',
+                bulletinText: 'Текст объявления ...',
+                bulletinImagees: [],
+            },
+            {
+                id: 3242346,
+                publishDate: 2342343,
+                publishDateString: '12:46, сегодня',
+                ownerId: 44334,
+                ownerLogin: 'Sashok',
+                bulletinSubject: 'Web-программист',
+                bulletinText: 'Текст объявления ...',
+                bulletinImagees: [],
+            },
+            {
+                id: 3284234,
+                publishDate: 2342343,
+                publishDateString: '12:46, сегодня',
+                ownerId: 44334,
+                ownerLogin: 'Sashok',
+                bulletinSubject: 'Web-программист',
+                bulletinText: 'Текст объявления ...',
+                bulletinImagees: [],
+            },
+            {
+                id: 32423466,
+                publishDate: 2342343,
+                publishDateString: '12:46, сегодня',
+                ownerId: 44334,
+                ownerLogin: 'Sashok',
+                bulletinSubject: 'Web-программист',
+                bulletinText: 'Текст объявления ...',
+                bulletinImagees: [],
+            },
+            {
+                id: 32423444,
+                publishDate: 2342343,
+                publishDateString: '12:46, сегодня',
+                ownerId: 44334,
+                ownerLogin: 'Sashok',
+                bulletinSubject: 'Web-программист',
+                bulletinText: 'Текст объявления ...',
+                bulletinImagees: [],
+            },
+            {
+                id: 32421134,
+                publishDate: 2342343,
+                publishDateString: '12:46, сегодня',
+                ownerId: 44334,
+                ownerLogin: 'Sashok',
+                bulletinSubject: 'Web-программист',
+                bulletinText: 'Текст объявления ...',
+                bulletinImagees: [],
+            },
+            {
+                id: 324544234,
+                publishDate: 2342343,
+                publishDateString: '12:46, сегодня',
+                ownerId: 44334,
+                ownerLogin: 'Sashok',
+                bulletinSubject: 'Web-программист',
+                bulletinText: 'Текст объявления ...',
+                bulletinImagees: [],
+            },
+            {
+                id: 324342234,
+                publishDate: 2342343,
+                publishDateString: '12:46, сегодня',
+                ownerId: 44334,
+                ownerLogin: 'Sashok',
+                bulletinSubject: 'Web-программист',
+                bulletinText: 'Текст объявления ...',
+                bulletinImagees: [],
+            },
+            {
+                id: 32423453454,
+                publishDate: 2342343,
+                publishDateString: '12:46, сегодня',
+                ownerId: 44334,
+                ownerLogin: 'Sashok',
+                bulletinSubject: 'Web-программист',
+                bulletinText: 'Текст объявления ...',
+                bulletinImagees: [],
+            },
         ])
     }, [])
 
@@ -45,19 +155,43 @@ function ModerationTape() {
 
     return (
         <div className={styles.moderationTape}>
-            {advertisements.map((advertisement) => (
-                <div
-                    className={
-                        activeAdvertisement?.id === advertisement.id
-                            ? [styles.item, styles.active].join(' ')
-                            : styles.item
-                    }
-                    key={advertisement.id}
-                    onClick={() => setActiveAdvertisement(advertisement)}
-                >
-                    <Advertisement advertisement={advertisement} />
+            <div className={styles.items}>
+                {advertisements.map((advertisement) => (
+                    <div
+                        className={
+                            activeAdvertisement?.id === advertisement.id
+                                ? [styles.item, styles.active].join(' ')
+                                : styles.item
+                        }
+                        key={advertisement.id}
+                        onClick={() => setActiveAdvertisement(advertisement)}
+                    >
+                        <Advertisement advertisement={advertisement} />
+                    </div>
+                ))}
+            </div>
+
+            <div className={styles.help}>
+                <div className={[styles.item, styles.green].join(' ')}>
+                    <span className={styles.action}>Одобрить</span>
+                    <span className={styles.key}>Пробел</span>
                 </div>
-            ))}
+
+                <div className={[styles.item, styles.orange].join(' ')}>
+                    <span className={styles.action}>Отклонить</span>
+                    <span className={styles.key}>Del</span>
+                </div>
+
+                <div className={[styles.item, styles.blue].join(' ')}>
+                    <span className={styles.action}>Эскалация</span>
+                    <span className={styles.key}>Shift+Enter</span>
+                </div>
+
+                <div className={styles.item}>
+                    <span className={styles.action}>Сохранить</span>
+                    <span className={styles.key}>F7</span>
+                </div>
+            </div>
         </div>
     )
 }
