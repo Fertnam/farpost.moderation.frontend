e4ee7b6fd43d71c1470768ee11f2aba14f7171a0
Fertnam <fertnam@mail.ru>
2021-12-29 19:10:30 +1000

    Рефакторинг, добавлен спиннер во время подгрузки объявлений

---
+++ src/components/Spinner/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './styles.scss'
+
+function Spinner() {
+    return (
+        <div className="loadingio-spinner-double-ring-0qbikhlz8b4">
+            <div className="ldio-eb449h7qvm5">
+                <div />
+                <div />
+                <div>
+                    <div />
+                </div>
+                <div>
+                    <div />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Spinner
--- src/containers/ModerationTape/components/List/index.tsx
+++ src/containers/ModerationTape/components/List/index.tsx
@@ -1,9 +1,16 @@
 import React, { useEffect, useState } from 'react'
 import List from 'components/List'
 import Advertisement from 'components/Advertisement'
+import Spinner from 'components/Spinner'
 import AdvertisementType from 'core/types/Advertisement'
 import useList from 'core/hooks/useList'
 import styles from './styles.module.scss'
+
+enum TapeStates {
+    EMPTY,
+    LOADING,
+    READY,
+}
 
 function ModerationTapeList() {
     const [
@@ -13,37 +20,42 @@ function ModerationTapeList() {
         setActiveAdvertisement,
     ] = useList<AdvertisementType>()
 
-    const [isDirty, setIsDirty] = useState<boolean>(true)
+    const [tapeState, setTapeState] = useState<TapeStates>(TapeStates.EMPTY)
 
     useEffect(function fetchAdvertisements() {
         function onKeyDown(event: KeyboardEvent) {
             if (!event.shiftKey && event.key === 'Enter') {
-                if (isDirty) {
-                    setIsDirty(false)
-                }
+                try {
+                    setTapeState(TapeStates.LOADING)
 
-                setAdvertisements([
-                    {
-                        id: 1234567,
-                        publishDate: 1234567,
-                        publishDateString: '08:46, сегодня',
-                        ownerId: 1234567,
-                        ownerLogin: 'Fertnam',
-                        bulletinSubject: 'UI/UX Designer (Владивосток)',
-                        bulletinText: 'Текст объявления ...',
-                        bulletinImagees: [],
-                    },
-                    {
-                        id: 324234,
-                        publishDate: 2342343,
-                        publishDateString: '12:46, сегодня',
-                        ownerId: 44334,
-                        ownerLogin: 'Sashok',
-                        bulletinSubject: 'Web-программист',
-                        bulletinText: 'Текст объявления ...',
-                        bulletinImagees: [],
-                    },
-                ])
+                    setTimeout(() => {
+                        setTapeState(TapeStates.READY)
+                        setAdvertisements([
+                            {
+                                id: 1234567,
+                                publishDate: 1234567,
+                                publishDateString: '08:46, сегодня',
+                                ownerId: 1234567,
+                                ownerLogin: 'Fertnam',
+                                bulletinSubject: 'UI/UX Designer (Владивосток)',
+                                bulletinText: 'Текст объявления ...',
+                                bulletinImagees: [],
+                            },
+                            {
+                                id: 324234,
+                                publishDate: 2342343,
+                                publishDateString: '12:46, сегодня',
+                                ownerId: 44334,
+                                ownerLogin: 'Sashok',
+                                bulletinSubject: 'Web-программист',
+                                bulletinText: 'Текст объявления ...',
+                                bulletinImagees: [],
+                            },
+                        ])
+                    }, 2000)
+                } catch (e) {
+                    console.error(e)
+                }
             }
         }
 
@@ -89,26 +101,34 @@ function ModerationTapeList() {
         [activeAdvertisement]
     )
 
-    return (
-        <div className={styles.moderationTapeList}>
-            {isDirty ? (
-                <div>Нажмите enter для загрузки объявлений</div>
-            ) : advertisements.length > 0 ? (
-                <List
-                    items={advertisements}
-                    activeItem={activeAdvertisement}
-                    renderItem={(advertisement) => (
-                        <Advertisement advertisement={advertisement} />
-                    )}
-                    onListItemClick={(advertisement) =>
-                        setActiveAdvertisement(advertisement)
-                    }
-                />
-            ) : (
-                <div>пусто</div>
-            )}
-        </div>
-    )
+    let tape = <div>В данный момент нет доступных для модерации объявлений</div>
+
+    if (tapeState === TapeStates.EMPTY) {
+        tape = (
+            <div>
+                Нажмите клавишу{' '}
+                <span style={{ fontWeight: 'bold' }}>Enter</span> для загрузки
+                объявлений
+            </div>
+        )
+    } else if (tapeState === TapeStates.LOADING) {
+        tape = <Spinner />
+    } else if (advertisements.length > 0) {
+        tape = (
+            <List
+                items={advertisements}
+                activeItem={activeAdvertisement}
+                renderItem={(advertisement) => (
+                    <Advertisement advertisement={advertisement} />
+                )}
+                onListItemClick={(advertisement) =>
+                    setActiveAdvertisement(advertisement)
+                }
+            />
+        )
+    }
+
+    return <div className={styles.moderationTapeList}>{tape}</div>
 }
 
 export default ModerationTapeList
